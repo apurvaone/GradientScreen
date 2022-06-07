9f1fc79cbf0fd3874a374aa9fa3604a8b0c30d99
apurvaone <57245377+apurvaone@users.noreply.github.com>
2022-06-07 11:48:19 +0530

    gradient animation completed

--- app/src/main/java/com/example/gradientscreen/MainActivity.kt
+++ app/src/main/java/com/example/gradientscreen/MainActivity.kt
@@ -1,11 +1,22 @@
 package com.example.gradientscreen
 
+import android.graphics.drawable.AnimationDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.constraintlayout.widget.ConstraintSet
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+
+        val animDrawable = findViewById<ConstraintLayout>(R.id.root_layout).background as AnimationDrawable
+        animDrawable.setEnterFadeDuration(10)
+        animDrawable.setExitFadeDuration(5000)
+        animDrawable.start()
     }
 }
